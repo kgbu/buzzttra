37f782f077668cff68432c1d848843912ead5cf8
kgbu <kgbu@www.jin.gr.jp>
2010-06-11 05:06:21 +0900

    buggy but get Buzz activities.

--- main.rb
+++ main.rb
@@ -5,12 +5,13 @@ require 'zlib'
 require 'stringio'
 
 configure do
-  KEY = "www.jin.gr.jp" 
-  SECRET = "1wsMBGB+QjfwFWFfbG4I/Se5"
+  Consumer_key = "www.jin.gr.jp" 
+  Consumer_secret = "1wsMBGB+QjfwFWFfbG4I/Se5"
 
-  SCOPE = 'https://www.googleapis.com/auth/buzz'
+  Scope = 'https://www.googleapis.com/auth/buzz'
   Request_token_url = 'https://www.google.com/accounts/OAuthGetRequestToken'
   Access_token_url = 'https://www.google.com/accounts/OAuthGetAccessToken'
+  Callback = 'http://www.jin.gr.jp/buzztra/access_token'
 
 
   # Session
@@ -23,6 +24,17 @@ configure do
 
 end
 
+before do
+  unless @client
+  @client = OAuthClient.new
+  @client.oauth_config.consumer_key = Consumer_key
+  @client.oauth_config.consumer_secret = Consumer_secret
+  @client.oauth_config.signature_method = 'HMAC-SHA1'
+  @client.oauth_config.http_method = :get
+  @client.debug_dev = STDERR if $DEBUG
+  end
+end
+  
 def base_url
   default_port = (request.scheme == "http") ? 80 : 443
   port = (request.port == default_port) ? "" : ":#{request.port.to_s}"
@@ -32,39 +44,51 @@ end
 
 
 get '/' do
-  erb %{ <a href="/buzztra/request_token">OAuth Login</a> }
+  erb %{ 
+  <a href="/buzztra/request_token">OAuth Login</a>
+  <a href="/buzztra/activities">Activities</a>
+  }
 end
 
 get '/login' do
   "#{base_url} Hello World!"
-
 end
 
+
+get '/activities' do
+# Access to a protected resource.
+# @consumption requires Buzz API
+content = @client.get_content("https://www.googleapis.com/buzz/v1/activities/@me/@consumption", :alt => :json, :prettyprint => true)
+  erb %{ <%= content %> }
+end
 
 get '/logout' do
 end
 
 get '/request_token' do
-callback = 'http://www.jin.gr.jp/buzztra/access_token'
-client = OAuthClient.new
-client.oauth_config.consumer_key = KEY
-client.oauth_config.consumer_secret = SECRET
-client.oauth_config.signature_method = 'HMAC-SHA1'
-client.oauth_config.http_method = :get
-client.debug_dev = STDERR if $DEBUG
+  # Get request token.
+  res = @client.get_request_token(Request_token_url, Callback, :scope => Scope)
+  token = res.oauth_params['oauth_token']
+  secret = res.oauth_params['oauth_token_secret']
+  raise if token.nil? or secret.nil?
 
-# Get request token.
-res = client.get_request_token(Request_token_url, callback, :scope => SCOPE)
-token = res.oauth_params['oauth_token']
-secret = res.oauth_params['oauth_token_secret']
-raise if token.nil? or secret.nil?
   session[:request_token] = token
   session[:request_token_secret] = secret
-  redirect request_token.authorize_url
+  
+  # You need to confirm authorization
+  redirect "https://www.google.com/buzz/api/auth/OAuthAuthorizeToken?oauth_token=#{token}&domain=#{Consumer_key}&scope=#{Scope}&btmpl=mobile"
+
 end
 
 
 get '/access_token' do
-  request_token = OAuth::RequestToken.new(
-    oauth_consumer, session[:request_token], session[:request_token_secret])
+  token = params[:oauth_token]
+  session[:request_token] = token
+  secret = session[:request_token_secret]
+  verifier = params[:oauth_verifier]
+
+  res = @client.get_access_token(Access_token_url, token, secret, verifier)
+  session[:user_id] = res.oauth_params['user_id']
+  content = @client.get_content("https://www.googleapis.com/buzz/v1/activities/@me/@consumption", :alt => :json, :prettyprint => true)
+  erb %{ <%= p res %> }
 end

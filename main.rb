8ca73ed4af9f1f565e5908f2eceeac09139c2c45
kgbu <kgbu@www.jin.gr.jp>
2010-06-21 06:19:10 +0900

    add comment APIs - not tested yet

--- main.rb
+++ main.rb
@@ -49,7 +49,13 @@ before do
     @client.oauth_config.secret = session[:access_secret]
   end
 end
+
+def base_url
+  "http://www." + Session_domain + Session_path
+end
   
+####################################################
+# authentication
 ####################################################
 
 get '/' do
@@ -94,12 +100,17 @@ get '/access_token' do
   session[:access_token] = @client.oauth_config.token
   session[:access_secret] = @client.oauth_config.secret
 
-  redirect "http://www." + Session_domain + Session_path
+  redirect base_url
 end
 
 
 ####################################################
+# Resource access 
+####################################################
 
+##
+#	activities
+#
 get '/activity/*/@self/*' do
   @contents = @client.get_content(APIbaseURL +
     "/activities/" + params[:splat][0] + "/@self/", params[:splat][1],
@@ -109,15 +120,15 @@ get '/activity/*/@self/*' do
 end
 
 get '/activities/' do
-  @contents = @client.get_content(APIbaseURL + "/activities/@me/@consumption", :alt => :json, :prettyprint => true)
+  @contents = @client.get_content(APIbaseURL + "/activities/@me/@consumption",
+    :alt => :json, :prettyprint => true)
 
   erb :contents
 end
 
 get '/activities/:name' do |id|
   if id && id.to_i > 0  then
-    @contents = @client.get_content(APIbaseURL +
-      "/activities/" + id.to_s + "/@public",
+    @contents = @client.get_content(APIbaseURL + "/activities/" + id.to_s + "/@public",
       :alt => :json, :prettyprint => true)
   end
 
@@ -125,20 +136,117 @@ get '/activities/:name' do |id|
 end
 
 post '/search' do
-  @contents = @client.get_content(APIbaseURL + "/activities/search", :alt => :json, :prettyprint => true, :q => params[:q])
+  @contents = @client.get_content(APIbaseURL + "/activities/search",
+    :alt => :json, :prettyprint => true, :q => params[:q])
 
   erb :contents
 end
 
 post '/activities' do
   @client.oauth_config.http_method = :post
-  text =<<__END__
+  body =<<__END__
  { "data": {
    "object": {
      "type": "note",
      "content": "#{params[:body]}"
    } } }
 __END__
-  @contents = @client.post_content(APIbaseURL + "/activities/@me/@self?alt=json", text)
+  extheader = { "Content-Type" => "application/json" }
+  @contents = @client.post_content(APIbaseURL + "/activities/@me/@self?alt=json", body, extheader)
+
+  if @contents
+    redirect base_url
+  else
+    @contents 
+  end
 end
 
+get '/activities/delete' do
+  @contents = @client.delete(APIbaseURL + "/activities/@me/@self/#{params[:id]}")
+  redirect base_url
+end
+
+get '/activities/like' do
+  @contents = @client.put(APIbaseURL + "/activities/@me/@liked/#{params[:id]}")
+  redirect base_url
+end
+
+get '/activities/unlike' do
+  @contents = @client.delete(APIbaseURL + "/activities/@me/@liked/#{params[:id]}")
+  redirect base_url
+end
+
+get '/activities/mute' do
+  @contents = @client.put(APIbaseURL + "/activities/@me/@muted/#{params[:id]}")
+  redirect base_url
+end
+
+get '/activities/unmute' do
+  @contents = @client.delete(APIbaseURL + "/activities/@me/@muted/#{params[:id]}")
+  redirect base_url
+end
+
+get '/activities/abuse' do
+  @contents = @client.put(APIbaseURL + "/activities/@me/@abuse/#{params[:id]}")
+  redirect base_url
+end
+
+##
+#	comments
+#
+get '/activities/@me/@self/*/@comments' do
+  @contents = @client.get_content(APIbaseURL +
+    "/activities/@me/@self/"+ params[:splat][0] + "/@comments",
+    :alt => :json, :prettyprint => true)
+
+  erb :contents
+end
+
+get '/activities/@me/@self/*/@comments/*' do
+  @contents = @client.get_content(APIbaseURL +
+    "/activities/@me/@self/"+ params[:splat][0] + "/@comments/" + params[:splat][1],
+    :alt => :json, :prettyprint => true)
+
+  erb :contents
+end
+
+post '/activities/@me/@self/@comments' do
+  body =<<__END__
+ { "data": {
+   "object": {
+     "content": "#{params[:body]}"
+   } } }
+__END__
+  extheader = { "Content-Type" => "application/json" }
+  @contents = @client.post_content(APIbaseURL +
+    "/activities/@me/@self/"+ params[:splat][0] + "/@comments?alt=json", body, extheader)
+
+  redirect base_url
+end
+
+post '/activities/@me/@self/@comments' do
+  body =<<__END__
+ { "data": {
+   "object": {
+     "content": "#{params[:body]}"
+   } } }
+__END__
+  extheader = { "Content-Type" => "application/json" }
+  @contents = @client.post_content(APIbaseURL +
+    "/activities/@me/@self/"+ params[:id] + "/@comments?alt=json", body, extheader)
+
+  redirect base_url
+end
+
+get '/activities/@me/@self/*/@comments/*' do
+  @contents = @client.get_content(APIbaseURL +
+    "/activities/@me/@self/"+ params[:splat][0] + "/@comments/" + params[:splat][1],
+    :alt => :json, :prettyprint => true)
+
+  erb :contents
+end
+
+get '/activities/comments/delete/' do
+  @contents = @client.delete(APIbaseURL + "/activities/@me/@self/#{params[:activityid]}/@comments/#{params[:commentid]}")
+  redirect base_url
+end
